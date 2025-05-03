1fa5007d1b47d56aa9bb8a9dac3eb24fc6af28f6
Cloyd Abad <abadcloyd@gmail.com>
2025-05-03 09:57:06 +0800

    finished navbar

---
+++ src/layouts/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+export default function Navbar() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h4>Global Happiness Report</h4>
+      </div>
+    </div>
+  );
+}
